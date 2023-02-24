51dd539468ed96ee62d726d5530651adc286af98
D1eg0Verdin <diego.verdinramirez23@ycdsbk12.ca>
2023-02-24 10:58:49 -0500

    yes

---
+++ src/UtilityTUnitesting/Main.java
@@ -0,0 +1,13 @@
+package UtilityTUnitesting;
+
+import UtilityTUnitesting.MathUtility;
+
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 1;
+        int num2 = 10;
+        int sum = MathUtility.add(num1, num2);
+        System.out.println("The sum of " + num2 + " and " + num2+ " is "+ sum);
+    }
+    
+}
---
+++ src/UtilityTUnitesting/MathUtility.java
@@ -0,0 +1,20 @@
+package UtilityTUnitesting;
+public class MathUtility{
+    /**
+     * a method that cinoytes the sum of integers
+     * @param num1
+     * @param num2
+     * @return the sum the two integers
+     */
+    public static int add(int num1, int num2){
+        return num1 + num2;
+    }
+
+    public static int substract(int num1, int num2){
+        return num1 - num2;
+    }
+
+    public static int multiply(int num1, int num2){
+        return num1 * num2;
+    }
+}   
